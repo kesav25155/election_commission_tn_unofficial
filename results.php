095ac8f2cd51e2853477e303d3df5c4cc1f94135
Kesav Kumar J <140180607+kesav25155@users.noreply.github.com>
2024-06-16 00:44:00 +0530

    Update results.php

--- results.php
+++ results.php
@@ -38,7 +38,18 @@ try {
     die();
 }
 ?>
-    <style>
+    
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Election Results</title>
+
+</head>
+<style>
      * {
                 margin: 0;
                 padding: 0;
@@ -357,17 +368,6 @@ try {
                     font-size: 14px;
                 }
         </style>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Election Results</title>
-
-</head>
-
 <body>
 <header>
         <br>
