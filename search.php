a9018ec7183d4b4fb917c65ade4c452b0884d809
Kesav Kumar J <140180607+kesav25155@users.noreply.github.com>
2024-06-16 10:08:57 +0530

    Update search.php

--- search.php
+++ search.php
@@ -385,7 +385,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                         <input type="text" id="lastName" name="lastName" placeholder="Enter last name">
                     </div>
                     <div class="light-box gender-radio">
-                        <label>Gender:</label>
+                        <label>Gender:</label><br>
                         <label><input type="radio" name="gender" value="male"> Male</label>
                         <label><input type="radio" name="gender" value="female"> Female</label>
                         <label><input type="radio" name="gender" value="other"> Other</label>
